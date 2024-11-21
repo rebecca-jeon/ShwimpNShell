5646cd9f73a38b23b5336cf626cf26f8d2d94790
Rebecca Jeon <rebeccajeon@Rebeccas-MacBook-Air-2.local>
2024-11-21 00:43:24 -0800

    Edit: Nav bar UI Design/ Added a New Font : TomatoKetchup

--- src/app/components/navbar.tsx
+++ src/app/components/navbar.tsx
@@ -3,11 +3,8 @@ import Link from 'next/link';
 
 const Navbar: React.FC = () => {
     return (
-        <nav>
-            <ul className='flex flex-row justify-between items-center'>
-                <li>
-                    <a href = "/">Logo</a>
-                </li>
+        <nav className='relative flex justify-between items-center pb-10 border-b '>
+            <ul className='flex flex-row items-center space-x-3 translate-y-5 text-[#FF5CB5]'>
                 <li>
                     <Link href = '/pages/shop'>Shop</Link>
                 </li>
@@ -21,6 +18,25 @@ const Navbar: React.FC = () => {
                     <Link href = '/pages/about'>About</Link>
                 </li>
             </ul>
+            <div className='absolute left-1/2 transform -translate-x-1/2'>
+                <img src = 'favicon.ico' alt = 'logo' className='h-12 w-auto translate-y-4'/>
+            </div>
+            <div>
+                <ul className='flex flex-row items-center space-x-3 translate-y-5'>
+                    <li>
+                        Insta
+                    </li>
+                    <li>
+                        Email
+                    </li>
+                    <li>
+                        User
+                    </li>
+                    <li>
+                        Basket
+                    </li>
+                </ul>
+            </div>
         </nav>
     );
 };
